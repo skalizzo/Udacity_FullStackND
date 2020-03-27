91f8b0d21fccc6392977ab91ea00ac0cfd412f44
Christian Richter <christian00richter@gmail.com>
2020-03-27 19:23:33 +0100

    checkboxen are now displayed correctly

--- todoapp/app.py
+++ todoapp/app.py
@@ -42,7 +42,7 @@ def create_todo():
   body = {}
   try:
     description = request.get_json()['description']
-    todo = Todo(description2=description)
+    todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
     body['description'] = todo.description
@@ -58,9 +58,24 @@ def create_todo():
     return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
 
